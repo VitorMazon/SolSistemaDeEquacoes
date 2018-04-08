30dc6f42e678b02db63773298d660cd80bd98fad
VitorMazon <31108097+VitorMazon@users.noreply.github.com>
2018-04-08 20:07:08 -0300

    TriangSup

--- Eliminacao_Gauss/gauss.c
+++ Eliminacao_Gauss/gauss.c
@@ -61,44 +61,13 @@ void subsreversa(double **M,double *raizes, int dim, int *n) {
 	}
 }
 
-void pivoteamento(double **M, int dim) {
-	int i, j, r, a, col, pivo, t;
-	double lamb, aux;
-	i=j=0;
-	puts("\nPivoteamento:\n");
-	for(j=0; j<dim-1; j++) {
-		//Acha o maior valor da coluna e o faz de pivo --->> corrigir
-		for(a=j; a<=dim-1; a++) {
-			for(t=j+1; t<=dim-1; t++) {
-				if(M[a][j] > M[t][j])
-					pivo = a;
-				else
-					pivo = t;
-			}
-		}
-		//Troca as linhas para o novo pivo
-		for(col=0; col<=dim; col++) {
-			aux=M[j][col];
-			M[j][col]=M[pivo][col];
-			M[pivo][col]=aux;
-		}
-		printf("Pivo para coluna %d\n",j+1);
-		imprime(M,dim);
-		for(i=j+1; i<=dim-1; i++) {
-			lamb=M[i][j]/M[j][j];
-			for(r=j; r<=dim; r++)
-				M[i][r] = M[i][r] - (lamb)*M[j][r];
-		}
-	}
-}
-
 int main(int argc, char **argv) {
 	double **M;
 	double *raizes;
 	int dim, i, ntrocas, noperacoes=0;
 	M=ler(argv[1], &dim);
 	imprime(M,dim);
-	pivoteamento(M,dim);
+	triangsup(M,dim);
 	puts("\nMatriz Solução:");
 	imprime(M,dim);
 	raizes = malloc(dim*sizeof(double));
